b6363f3cb3dd0704d07c1a6c654e36c160c9d88b
unknown <elmiosman343@gmail.com>
2023-03-29 09:52:28 -0900

    first commit

---
+++ src/Component/Center.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Center() {
+  return (
+    <div className='Center'>
+      <h1>MY NAME: Abdifitah abdirahman nunow</h1>
+      <h1>mY ID C120285</h1>
+    </div>
+  );
+}
+
+export default Center;
